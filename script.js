97580e1f3ac7b32cd01567e8bf68683c75d2f0c8
Rishabh <rishabh918263@gmail.com>
2025-04-24 21:09:58 +0530

    document function

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
+// Notes-
+
 // console.log(`Hello`);
 // console.log(`I like Pizza!`);
 
 // window.alert(`This in an alert!`)
 // window.alert(`Be Cautious!`)
+
+// document.getElementById("MyH1").textContent = `Hello`;
+// document.getElementById("MyP").textContent = `Beware`;
 
 // This is a comment
 
