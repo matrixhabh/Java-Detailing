176b65a9e554ce836c217ce47b19bedb0acd463a
Rishabh <rishabh918263@gmail.com>
2025-04-24 21:04:11 +0530

    comments

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-console.log(`Hello`);
-console.log(`I like Pizza!`);
+// console.log(`Hello`);
+// console.log(`I like Pizza!`);
+
+// window.alert(`This in an alert!`)
+// window.alert(`Be Cautious!`)
+
+// This is a comment
+
+/*
+    This
+    is 
+    a
+    comment
+*/ 
